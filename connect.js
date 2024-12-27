0f44b9ec3d8d0de32fb717ce45cfd0e780a7b57a
MoreAndMore <abulljk@gmail.com>
2024-12-27 17:15:26 +0500

    switching port to ganache http

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 // Import Web3 library
 const { Web3 } = require('web3');
 
-const provider = new Web3.providers.HttpProvider('http://localhost:8545');
+const provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545');
 
 const web3 = new Web3(provider);
 
